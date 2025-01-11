d0a96fe5df9d812675e06168151def6ca6e8eb63
Daniel Adigun <121884453+danieladigun234@users.noreply.github.com>
2025-01-11 19:14:37 +0000

    WIP - project setup and mock api

---
+++ Server/server.js
@@ -0,0 +1,28 @@
+﻿const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/api/students', (req, res) => {
+  res.json([{
+    id: 1,
+    name: 'Jomiloju',
+    course: 'Software Engineering',
+    level: '400',
+    school: 'Oxford'
+  },{
+    id: 2,
+    name: 'Daniel',
+    course: 'Electrical Engineering',
+    level: '100',
+    school: 'University of Lagos'
+  }])
+})
+
+// POST -> api/students/add
+// UPDATE
+
+// Designing specifications for our booking application.
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
