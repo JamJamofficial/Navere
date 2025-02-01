baaeac5437943465832f272c410269592386a338
Jamdog <113641391+Jamdogofficial@users.noreply.github.com>
2025-02-01 18:29:30 +0000

    Mock api to return calendar

--- Server/server.js
+++ Server/server.js
@@ -1,27 +1,9 @@
 ﻿const express = require('express')
 const app = express()
 const port = 3000
-
-app.get('/api/students', (req, res) => {
-  res.json([{
-    id: 1,
-    name: 'Jomiloju',
-    course: 'Software Engineering',
-    level: '400',
-    school: 'Oxford'
-  },{
-    id: 2,
-    name: 'Daniel',
-    course: 'Electrical Engineering',
-    level: '100',
-    school: 'University of Lagos'
-  }])
-})
-
-// POST -> api/students/add
-// UPDATE
-
-// Designing specifications for our booking application.
+app.get('/api/calendar', (req, res) => {
+  return res.json({daysOfTheWeek: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
