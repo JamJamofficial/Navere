656660f269b041ca0107416105c164055be904dd
Jamdog <113641391+Jamdogofficial@users.noreply.github.com>
2025-02-08 18:30:09 +0000

    Renamed http server to application name

--- Server/server.js
+++ Server/server.js
@@ -9,5 +9,5 @@ app.get('/api/calendar', (req, res) => {
 
 })
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Navere server is listening on port ${port}`)
 })
